1e91a75af72bf505aad3703979bb6d3d44c7e3c2
EGOIST <0x142857@gmail.com>
2021-03-17 20:37:35 +0800

    fix: bundle @vue/compiler-sfc

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,6 @@
 import { Options } from 'tsup'
+import resolve from 'resolve-from'
+import { builtinModules } from 'module'
 
 const options: Options = {
   esbuildPlugins: [
@@ -8,6 +10,26 @@ const options: Options = {
       setup(build) {
         build.onResolve({ filter: /\.\/typescript/ }, (args) => {
           return { path: require.resolve('typescript') }
+        })
+      },
+    },
+    {
+      // Some modules like `pug` are required in consolidate but should not be bundled
+      name: 'ignore-require-in-consolidate',
+      setup(build) {
+        build.onResolve({ filter: /^[a-z]/ }, (args) => {
+          if (builtinModules.includes(args.path))
+            return {
+              path: args.path,
+              external: true,
+            }
+          if (args.importer.includes('consolidate')) {
+            const id = resolve.silent(args.resolveDir, args.path)
+            if (id) {
+              return { path: id }
+            }
+            return { path: args.path, external: true }
+          }
         })
       },
     },
