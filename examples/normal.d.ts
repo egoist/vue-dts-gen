4fe43d024b0318a37a30a6b47a718cf91085958c
EGOIST <0x142857@gmail.com>
2021-08-22 13:12:02 +0000

    chore: remove package-lock.json

--- examples/normal.d.ts
+++ examples/normal.d.ts
@@ -5,8 +5,6 @@ declare const _default: import("vue").DefineComponent<{
         required: true;
     };
 }, unknown, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
-    start?: unknown;
-} & {
     start: number;
-} & {}> & {}, {}>;
+} & {}>, {}>;
 export default _default;
